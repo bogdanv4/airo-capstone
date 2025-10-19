751276a408dc25bb6b0fb950296b1e98e1496334
Bogdan Vujić <vujic.bogdan@gmail.com>
2025-10-19 15:42:40 +0200

    fix/design-changes (#9)
    * Implement small design changes
    
    * Small code refactor

--- frontend/src/constants/const.js
+++ frontend/src/constants/const.js
@@ -1,6 +1,3 @@
-export const USER_INFO_API_URL =
-  'https://www.googleapis.com/oauth2/v3/userinfo';
-
 export const MAP_STYLE_URL = `https://api.maptiler.com/maps/01993d9d-f8af-7979-a2e2-6b3ce864642a/style.json?key=${import.meta.env.VITE_MAPTILER_API_KEY}`;
 
 export const CORDS_TO_LOCATION_BASE_URL = `https://nominatim.openstreetmap.org/reverse`;
